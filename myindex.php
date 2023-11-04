86955c4ee91b48ce830143391185e6a97ba3bf6c
pranayjain099 <pranayjain2001@gmail.com>
2023-11-04 06:29:51 +0530

    Home page desin completed all styling done

--- myindex.php
+++ myindex.php
@@ -398,7 +398,7 @@
 
   <h2 class="mt-4 pt-4 mb-4 text-center fw-bold h-font">Testimonials</h2>
 
-  <div class="container">
+  <div class="container mt-5">
     <div class="swiper swiper-testimonials">
       <div class="swiper-wrapper mb-5">
         <div class="swiper-slide bg-white mb-3">
@@ -459,54 +459,147 @@
     </div>
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-  <script>
-    var swiper = new Swiper(".swiper-container", {
-      spaceBetween: 30,
-      effect: "fade",
-      loop: "true",
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      }
-    });
+  <div class="col-lg-12 text-center mt-5">
+    <a href="# " class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">Know More >>></a>
+  </div>
+  <!-- Reach Us -->
 
-    var swiper = new Swiper(".swiper-testimonials", {
-      effect: "coverflow",
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: "auto",
-      loop: true,
-      slidesPerView: "3",
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-      },
-      breakpoints: {
-        320: {   // for 320 px devices
-          slidesPerView: 1,
+  <h2 class="mt-4 pt-4 mb-4 text-center fw-bold h-font">Reach Us</h2>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+        <iframe class="w-100 rounded" height="360px"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d121059.0344739699!2d73.86296739999999!3d18.52461645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf2e67461101%3A0x828d43bf9d9ee343!2sPune%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1699056721499!5m2!1sen!2sin"
+          loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+
+      <div class="col-lg-4 col-md-4">
+        <!-- Telephone Number -->
+        <div class="bg-white p-4 rounded mb-4">
+          <h5>Call Us</h5>
+          <a href="tel:+9179980090" class="d-inline-block mb-2 text-decoration-none text-dark"><i
+              class="bi bi-telephone-plus"></i> +9179980090</a>
+          <br>
+          <a href="tel:+9179980090" class="d-inline-block text-decoration-none text-dark"><i
+              class="bi bi-telephone-plus"></i> +9179980090</a>
+        </div>
+        <!-- Follow US -->
+        <div class="bg-white p-4 rounded mb-4">
+          <h5>Follow Us</h5>
+          <a href="#" class="d-inline-block mb-3">
+            <span class="badge bg-light text-dark fs-6 p-2">
+              <i class="bi bi-twitter me-1"></i>Twitter
+            </span>
+          </a>
+          <br>
+          <a href="#" class="d-inline-block mb-3">
+            <span class="badge bg-light text-dark fs-6 p-2">
+              <i class="bi bi-facebook me-1"></i>Facebook
+            </span>
+          </a>
+          <br>
+          <a href="#" class="d-inline-block mb-3">
+            <span class="badge bg-light text-dark fs-6 p-2">
+              <i class="bi bi-instagram me-1"></i>Instagram
+            </span>
+          </a>
+          <br>
+
+
+
+
+        </div>
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <div class="container-fluid bg-white mt-5">
+      <div class="row">
+        <div class="col-lg-4 p-4">
+          <div class="h-font fw-bold fs-3 mb-2">7Star</div>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum amet blanditiis voluptatibus dolorum id
+            nobis aut molestiae quas! Officia provident repellendus sapiente quod, maiores aliquid corrupti aperiam
+            porro voluptatibus doloribus.</p>
+        </div>
+        <div class="col-lg-4 p-4">
+          <h5 class="mb-3">Links</h5>
+          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
+          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a><br>
+          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+        </div>
+        <div class="col-lg-4 p-4">
+          <h5 class="mb-3">Follow Us</h5>
+          <a href="#" class="d-inline-block text-dark text-decoration-none mb-3">
+            <i class="bi bi-twitter me-1"></i>Twitter
+          </a>
+          <br>
+          <a href="#" class="d-inline-block text-dark text-decoration-none mb-3">
+            <i class="bi bi-facebook me-1"></i>Facebook
+          </a>
+          <br>
+          <a href="#" class="d-inline-block text-dark text-decoration-none mb-3">
+            <i class="bi bi-instagram me-1"></i>Instagram
+          </a>
+          <br>
+        </div>
+      </div>
+    </div>
+
+    <h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by Pranay Jain</h6>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script>
+      var swiper = new Swiper(".swiper-container", {
+        spaceBetween: 30,
+        effect: "fade",
+        loop: "true",
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        }
+      });
+
+      var swiper = new Swiper(".swiper-testimonials", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        loop: true,
+        slidesPerView: "3",
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: false,
         },
-        640: {
-          slidesPerView: 2,
+        autoplay: {
+          delay: 4000,
+          disableOnInteraction: false,
         },
-        768: {
-          slidesPerView: 2,
+        pagination: {
+          el: ".swiper-pagination",
         },
-        1024: {
-          slidesPerView: 3,
-        },
-      }
-    });
-  </script>
+        breakpoints: {
+          320: {   // for 320 px devices
+            slidesPerView: 1,
+          },
+          640: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        }
+      });
+    </script>
 </body>
 
 </html>
