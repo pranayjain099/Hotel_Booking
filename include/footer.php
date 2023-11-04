6ee933b5bc0f6c06aeb6326f86694b5532ace99b
pranayjain099 <pranayjain2001@gmail.com>
2023-11-04 14:54:53 +0530

    new file created and included for common header and footer

---
+++ include/footer.php
@@ -0,0 +1,36 @@
+<!-- Footer -->
+<div class="container-fluid bg-white mt-5">
+    <div class="row">
+        <div class="col-lg-4 p-4">
+            <div class="h-font fw-bold fs-3 mb-2">7Star</div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum amet blanditiis voluptatibus dolorum id
+                nobis aut molestiae quas! Officia provident repellendus sapiente quod, maiores aliquid corrupti aperiam
+                porro voluptatibus doloribus.</p>
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Links</h5>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Follow Us</h5>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-3">
+                <i class="bi bi-twitter me-1"></i>Twitter
+            </a>
+            <br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-3">
+                <i class="bi bi-facebook me-1"></i>Facebook
+            </a>
+            <br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-3">
+                <i class="bi bi-instagram me-1"></i>Instagram
+            </a>
+            <br>
+        </div>
+    </div>
+</div>
+
+<h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by Pranay Jain</h6>
--- myindex.php
+++ myindex.php
@@ -380,42 +380,11 @@
       </div>
     </div>
 
-    <!-- Footer -->
-    <div class="container-fluid bg-white mt-5">
-      <div class="row">
-        <div class="col-lg-4 p-4">
-          <div class="h-font fw-bold fs-3 mb-2">7Star</div>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum amet blanditiis voluptatibus dolorum id
-            nobis aut molestiae quas! Officia provident repellendus sapiente quod, maiores aliquid corrupti aperiam
-            porro voluptatibus doloribus.</p>
-        </div>
-        <div class="col-lg-4 p-4">
-          <h5 class="mb-3">Links</h5>
-          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
-          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
-          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
-          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a><br>
-          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
-        </div>
-        <div class="col-lg-4 p-4">
-          <h5 class="mb-3">Follow Us</h5>
-          <a href="#" class="d-inline-block text-dark text-decoration-none mb-3">
-            <i class="bi bi-twitter me-1"></i>Twitter
-          </a>
-          <br>
-          <a href="#" class="d-inline-block text-dark text-decoration-none mb-3">
-            <i class="bi bi-facebook me-1"></i>Facebook
-          </a>
-          <br>
-          <a href="#" class="d-inline-block text-dark text-decoration-none mb-3">
-            <i class="bi bi-instagram me-1"></i>Instagram
-          </a>
-          <br>
-        </div>
-      </div>
-    </div>
+    <!-- including Footer -->
+    <?php
+    require('include/footer.php')
+      ?>
 
-    <h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by Pranay Jain</h6>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
       crossorigin="anonymous"></script>
