23ecd219b18b36070cc0f8acb753fa5cc653f9fb
pranayjain099 <pranayjain2001@gmail.com>
2023-11-07 23:59:08 +0530

    Connection established with the database

--- Admin/include/db_config.php
+++ Admin/include/db_config.php
@@ -1,13 +1,16 @@
 <!-- This is the main file for all the crud operations , All the queries will be fired from here only , We will make dynamic queries , queries that will filter the data-->
 
 <?php
-
+// Connecting to the Database
 $hname = 'localhost'; // Hostname
 $uname = 'root'; // username
 $pass = "";
 $db = '7_star';
 
+// Create a connection
 $con = mysqli_connect($hname, $uname, $pass, $db);
+
+// Die if connection was not successful
 if (!$con) {
     die('Cannot connect to database' . mysqli_connect_error());
 }
