6458a00828ca236cfed272f72c1f5f12200508f5
pranayjain099 <pranayjain2001@gmail.com>
2023-11-22 23:28:10 +0530

    General Setting Card done

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -25,7 +25,6 @@ session_regenerate_id(true);
     <div class="container-fluid" id="main-content">
         <div class="row">
             <div class="col-lg-10 ms-auto p-4 overflow-hidden">
-
             </div>
         </div>
     </div>
