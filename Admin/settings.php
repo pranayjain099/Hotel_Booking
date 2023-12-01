78daf1e4b7f15c214b1460f9a82b0d4f39f00d63
pranayjain099 <pranayjain2001@gmail.com>
2023-12-02 00:44:54 +0530

    Management Team Section started

--- Admin/settings.php
+++ Admin/settings.php
@@ -24,7 +24,7 @@ session_regenerate_id(true);
     <!-- Main content  -->
     <div class="container-fluid" id="main-content">
         <div class="row">
-            <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+            <div class="col-lg-10 ms-auto p-4 pt-0 overflow-hidden">
                 <h3 class="mb-4">SETTINGS</h3>
                 <!-- General Settings Section -->
                 <div class="card border-0 shadow-sm mb-4">
@@ -257,197 +257,246 @@ session_regenerate_id(true);
 
                     </div>
                 </div>
+
+                <!-- Management Team Section -->
+                <div class="card border-0 shadow-sm mb-4">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h5 class="card-title m-0">Management Team</h5>
+                            <button type="button" class="btn btn-dark shadow-none btn-sm" data-bs-toggle="modal"
+                                data-bs-target="#team-s">
+                                <i class="bi bi-plus-square"></i> Add
+                            </button>
+                        </div>
+
+                        <div class="row" id="team-data">
+
+                        </div>
+                    </div>
+                </div>
+                <!-- Management Team Modal -->
+                <div class="modal fade" id="team-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1"
+                    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <form id="general_s_form">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">General Settings</h5>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="mb-3">
+                                        <label class="form-label fw-bold">Site Title</label>
+                                        <input type="text" name="site_title" id="site_title_input"
+                                            class="form-control shadow-none" required>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label class="form-label fw-bold">About us</label>
+                                        <textarea name="site_about" id="site_about_input"
+                                            class="form-control shadow-none" rows="6" required></textarea>
+                                    </div>
+                                </div>
+
+                                <!-- When we click on edit and change the site tile and about but we changed and do cancel then data gets saved but we dont want this so here we put onlick and waha ki value ko databse mein jo hai whi set krdi -->
+                                <div class="modal-footer">
+                                    <button type="button" onclick="site_title.value = general_data.site_title ,
+                                        site_about.value = general_data.site_about"
+                                        class="btn text-secondary shadow-none" data-bs-dismiss="modal">CANCEL</button>
+                                    <button type="submit" class="btn custom-bg text-white shadow-none">SUBMIT</button>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
+
+    <?php require('include/script.php'); ?>
+
+    <script>
+
+        // we will store data of general setting modal
+        let general_data, contacts_data;
+
+        let general_s_form = document.getElementById('general_s_form');
+        let site_title_input = document.getElementById('site_title_input');
+        let site_about_input = document.getElementById('site_about_input');
+        let contacts_s_form = document.getElementById('contacts_s_form');
+
+        general_s_form.addEventListener('submit', function (e) {
+            e.preventDefault(); // Will avoid page from refresh means will avoid form to submit.
+            update_general(site_title_input.value, site_about_input.value)
+
+        })
 
 
-        <?php
-        require('include/script.php');
-        ?>
+        function get_general() {
+            let site_title = document.getElementById('site_title');
+            let site_about = document.getElementById('site_about');
 
-        <script>
+            let shutdown_toggle = document.getElementById('shutdown-toggle')
 
-            // we will store data of general setting modal
-            let general_data, contacts_data;
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/settings_crud.php", true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');  // we have to apply this header when we send data thorugh post.
 
-            let general_s_form = document.getElementById('general_s_form');
-            let site_title_input = document.getElementById('site_title_input');
-            let site_about_input = document.getElementById('site_about_input');
-            let contacts_s_form = document.getElementById('contacts_s_form');
+            // data recieved here now 
 
-            general_s_form.addEventListener('submit', function (e) {
-                e.preventDefault(); // Will avoid page from refresh means will avoid form to submit.
-                update_general(site_title_input.value, site_about_input.value)
+            // xhr.onreadystatechange = function{
+            //     if(this.readyState==4 && this.status==200){
 
-            })
+            //     }
+            // }
+            // you can write like this but to write this in short there is a way
 
+            xhr.onload = function () {
+                // JSON.parse converts json data into js object.
+                general_data = JSON.parse(this.responseText);
 
-            function get_general() {
-                let site_title = document.getElementById('site_title');
-                let site_about = document.getElementById('site_about');
+                site_title.innerText = general_data.site_title;
+                site_about.innerText = general_data.site_about;
 
-                let shutdown_toggle = document.getElementById('shutdown-toggle')
+                site_title_input.value = general_data.site_title;
+                site_about_input.value = general_data.site_about;
 
-                let xhr = new XMLHttpRequest();
-                xhr.open("POST", "ajax/settings_crud.php", true);
-                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');  // we have to apply this header when we send data thorugh post.
-
-                // data recieved here now 
-
-                // xhr.onreadystatechange = function{
-                //     if(this.readyState==4 && this.status==200){
-
-                //     }
-                // }
-                // you can write like this but to write this in short there is a way
-
-                xhr.onload = function () {
-                    // JSON.parse converts json data into js object.
-                    general_data = JSON.parse(this.responseText);
-
-                    site_title.innerText = general_data.site_title;
-                    site_about.innerText = general_data.site_about;
-
-                    site_title_input.value = general_data.site_title;
-                    site_about_input.value = general_data.site_about;
-
-                    // It means site is up.
-                    // we will recieve 3 data in general_data site_tite,site_about and shutdown and shutdown is boolean so its either 0 or 1.
-                    // 1 means site is down
-                    if (general_data.shutdown == 0) {
-                        shutdown_toggle.checked = false;
-                        shutdown_toggle.value = 0;
-                    } else {
-                        shutdown_toggle.checked = true;
-                        shutdown_toggle.value = 1;
-                    }
+                // It means site is up.
+                // we will recieve 3 data in general_data site_tite,site_about and shutdown and shutdown is boolean so its either 0 or 1.
+                // 1 means site is down
+                if (general_data.shutdown == 0) {
+                    shutdown_toggle.checked = false;
+                    shutdown_toggle.value = 0;
+                } else {
+                    shutdown_toggle.checked = true;
+                    shutdown_toggle.value = 1;
                 }
-                xhr.send('get_general');
             }
+            xhr.send('get_general');
+        }
 
 
 
-            function update_general(site_title_val, site_about_val) {
-                let xhr = new XMLHttpRequest();
-                xhr.open("POST", "ajax/settings_crud.php", true);
-                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        function update_general(site_title_val, site_about_val) {
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/settings_crud.php", true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-                xhr.onload = function () {
+            xhr.onload = function () {
 
-                    // to hide modal
-                    var myModal = document.getElementById('general-s');
-                    var modal = bootstrap.Modal.getInstance(myModal);
-                    modal.hide();
+                // to hide modal
+                var myModal = document.getElementById('general-s');
+                var modal = bootstrap.Modal.getInstance(myModal);
+                modal.hide();
 
-                    // updating without reloading
-                    if (this.responseText == 1) {
-                        alert('success', 'Changes saved!');
-                        get_general();
-                    } else {
-                        alert('error', 'No Changes saved!');
-                    }
-
-
-
-                }
-                xhr.send('site_title=' + site_title_val + '&site_about=' + site_about_val + '&update_general');
-            }
-
-            function update_shutdown(val) {
-                let xhr = new XMLHttpRequest();
-                xhr.open("POST", "ajax/settings_crud.php", true);
-                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-                xhr.onload = function () {
-
-                    if (this.responseText == 1 && general_data.shutdown == 0) {
-                        alert('success', 'Site has been shutdown');
-                    } else {
-                        alert('success', 'Shutdown mode off !');
-                    }
+                // updating without reloading
+                if (this.responseText == 1) {
+                    alert('success', 'Changes saved!');
                     get_general();
-
-
+                } else {
+                    alert('error', 'No Changes saved!');
                 }
-                xhr.send('update_shutdown=' + val);
-            }
-
-            function get_contacts() {
-
-                // on]ption one either create 8 9 variable jitne table mein hai then doc.getelemntbyid kro else create a array.
-                // storing ids of all the tags 
-                let contacts_p_id = ['address', 'gmap', 'pn1', 'pn2', 'email', 'fb', 'insta', 'tw'];
-                let iframe = document.getElementById('iframe');
-
-                let xhr = new XMLHttpRequest();
-                xhr.open("POST", "ajax/settings_crud.php", true);
-                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-                xhr.onload = function () {
-                    contacts_data = JSON.parse(this.responseText);
-                    contacts_data = Object.values(contacts_data);
-
-                    for (i = 0; i < contacts_p_id.length; i++) {
-                        document.getElementById(contacts_p_id[i]).innerText = contacts_data[i + 1];
-                    }
-                    iframe.src = contacts_data[9];
-                    contacts_input(contacts_data);
-
-                }
-                xhr.send('get_contacts');
-            }
-
-            function contacts_input(data) {
-                let contacts_input_id = ['address_input', 'gmap_input', 'pn1_input', 'pn2_input', 'email_input', 'fb_input', 'insta_input', 'tw_input', 'iframe_input'];
-
-                for (i = 0; i < contacts_input_id.length; i++) {
-                    document.getElementById(contacts_input_id[i]).value = data[i + 1];
-                }
-            }
-
-            contacts_s_form.addEventListener('submit', function (e) {
-                e.preventDefault();
-                update_contacts();
-            })
-
-            // Update contacts
-            function update_contacts() {
-                let index = ['address', 'gmap', 'pn1', 'pn2', 'email', 'fb', 'insta', 'tw', 'iframe'];
-                let contacts_input_id = ['address_input', 'gmap_input', 'pn1_input', 'pn2_input', 'email_input', 'fb_input', 'insta_input', 'tw_input', 'iframe_input'];
-
-                let data_str = "";
-
-                for (i = 0; i < index.length; i++) {
-                    data_str += index[i] + "=" + document.getElementById(contacts_input_id[i]).value + '&';
-                }
-                data_str += "update_contacts";
-
-                let xhr = new XMLHttpRequest();
-                xhr.open("POST", "ajax/settings_crud.php", true);
-                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-                xhr.onload = function () {
-                    var myModal = document.getElementById('contacts-s');
-                    var modal = bootstrap.Modal.getInstance(myModal);
-                    modal.hide();
-                    if (this.responseText == 1) {
-                        alert('success', 'Changes Saved');
-                        get_contacts();
-                    } else {
-                        alert('error', 'No changes');
-                    }
 
 
-                }
-                xhr.send(data_str);
 
             }
-            // when we load window then this function will be called
-            window.onload = function () {
+            xhr.send('site_title=' + site_title_val + '&site_about=' + site_about_val + '&update_general');
+        }
+
+        function update_shutdown(val) {
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/settings_crud.php", true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+            xhr.onload = function () {
+
+                if (this.responseText == 1 && general_data.shutdown == 0) {
+                    alert('success', 'Site has been shutdown');
+                } else {
+                    alert('success', 'Shutdown mode off !');
+                }
                 get_general();
-                get_contacts();
+
+
             }
-        </script>
+            xhr.send('update_shutdown=' + val);
+        }
+
+        function get_contacts() {
+
+            // on]ption one either create 8 9 variable jitne table mein hai then doc.getelemntbyid kro else create a array.
+            // storing ids of all the tags 
+            let contacts_p_id = ['address', 'gmap', 'pn1', 'pn2', 'email', 'fb', 'insta', 'tw'];
+            let iframe = document.getElementById('iframe');
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/settings_crud.php", true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+            xhr.onload = function () {
+                contacts_data = JSON.parse(this.responseText);
+                contacts_data = Object.values(contacts_data);
+
+                for (i = 0; i < contacts_p_id.length; i++) {
+                    document.getElementById(contacts_p_id[i]).innerText = contacts_data[i + 1];
+                }
+                iframe.src = contacts_data[9];
+                contacts_input(contacts_data);
+
+            }
+            xhr.send('get_contacts');
+        }
+
+        function contacts_input(data) {
+            let contacts_input_id = ['address_input', 'gmap_input', 'pn1_input', 'pn2_input', 'email_input', 'fb_input', 'insta_input', 'tw_input', 'iframe_input'];
+
+            for (i = 0; i < contacts_input_id.length; i++) {
+                document.getElementById(contacts_input_id[i]).value = data[i + 1];
+            }
+        }
+
+        contacts_s_form.addEventListener('submit', function (e) {
+            e.preventDefault();
+            update_contacts();
+        })
+
+        // Update contacts
+        function update_contacts() {
+            let index = ['address', 'gmap', 'pn1', 'pn2', 'email', 'fb', 'insta', 'tw', 'iframe'];
+            let contacts_input_id = ['address_input', 'gmap_input', 'pn1_input', 'pn2_input', 'email_input', 'fb_input', 'insta_input', 'tw_input', 'iframe_input'];
+
+            let data_str = "";
+
+            for (i = 0; i < index.length; i++) {
+                data_str += index[i] + "=" + document.getElementById(contacts_input_id[i]).value + '&';
+            }
+            data_str += "update_contacts";
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/settings_crud.php", true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+            xhr.onload = function () {
+                var myModal = document.getElementById('contacts-s');
+                var modal = bootstrap.Modal.getInstance(myModal);
+                modal.hide();
+                if (this.responseText == 1) {
+                    alert('success', 'Changes Saved');
+                    get_contacts();
+                } else {
+                    alert('error', 'No changes');
+                }
+
+
+            }
+            xhr.send(data_str);
+
+        }
+        // when we load window then this function will be called
+        window.onload = function () {
+            get_general();
+            get_contacts();
+        }
+    </script>
 </body>
 
 </html>
