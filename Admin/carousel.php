d10b8293cbc8d676646c222dc60e56dcff9a6e88
pranayjain099 <pranayjain2001@gmail.com>
2023-12-08 17:17:08 +0530

    bug fixed

--- Admin/carousel.php
+++ Admin/carousel.php
@@ -4,13 +4,14 @@ adminLogin();
 session_regenerate_id(true);
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Settings</title>
+    <title>Admin - Carousel</title>
     <?php
     require('include/links.php');
     ?>
@@ -24,256 +25,25 @@ session_regenerate_id(true);
     <div class="container-fluid" id="main-content">
         <div class="row">
             <div class="col-lg-10 ms-auto p-4 pt-0 overflow-hidden">
-                <h3 class="mb-4">SETTINGS</h3>
-                <!-- General Settings Section -->
+                <h3 class="mb-4">CAROUSEL</h3>
+
+                <!-- CAROUSEL Section -->
                 <div class="card border-0 shadow-sm mb-4">
                     <div class="card-body">
                         <div class="d-flex align-items-center justify-content-between mb-3">
-                            <h5 class="card-title m-0">General Settings</h5>
+                            <h5 class="card-title m-0">Images</h5>
                             <button type="button" class="btn btn-dark shadow-none btn-sm" data-bs-toggle="modal"
-                                data-bs-target="#general-s">
-                                <i class="bi bi-pencil-square"></i> Edit
-                            </button>
-                        </div>
-                        <h6 class="card-subtitle mb-1 fw-bold">Site Title</h6>
-                        <p class="card-text" id="site_title"></p>
-                        <h6 class="card-subtitle mb-1 fw-bold">About us</h6>
-                        <p class="card-text" id="site_about"></p>
-                    </div>
-                </div>
-                <!-- General settings Modal -->
-                <div class="modal fade" id="general-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1"
-                    aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <form id="general_s_form">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title">General Settings</h5>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="mb-3">
-                                        <label class="form-label fw-bold">Site Title</label>
-                                        <input type="text" name="site_title" id="site_title_input"
-                                            class="form-control shadow-none" required>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label class="form-label fw-bold">About us</label>
-                                        <textarea name="site_about" id="site_about_input"
-                                            class="form-control shadow-none" rows="6" required></textarea>
-                                    </div>
-                                </div>
-
-                                <!-- When we click on edit and change the site tile and about but we changed and do cancel then data gets saved but we dont want this so here we put onlick and waha ki value ko databse mein jo hai whi set krdi -->
-                                <div class="modal-footer">
-                                    <button type="button" onclick="site_title.value = general_data.site_title ,
-                                        site_about.value = general_data.site_about"
-                                        class="btn text-secondary shadow-none" data-bs-dismiss="modal">CANCEL</button>
-                                    <button type="submit" class="btn custom-bg text-white shadow-none">SUBMIT</button>
-                                </div>
-                            </div>
-                        </form>
-
-                    </div>
-                </div>
-
-                <!-- Shutdown section (Site down krna ki koi booking na kar paye)-->
-                <div class="card border-0 shadow-sm mb-4">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center justify-content-between mb-3">
-                            <h5 class="card-title m-0">Shutdown Website</h5>
-                            <!-- This is switch taken from bootstrap -->
-                            <div class="form-check form-switch">
-                                <form>
-                                    <input onchange="update_shutdown(this.value)" class="form-check-input"
-                                        type="checkbox" id="shutdown-toggle">
-                                </form>
-                            </div>
-                        </div>
-                        <p class="card-text">No customers will be allowed to book hotel room , when shutdown mode is
-                            turned on</p>
-                    </div>
-                </div>
-
-                <!-- Contact Detail Section -->
-                <div class="card border-0 shadow-sm mb-4">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center justify-content-between mb-3">
-                            <h5 class="card-title m-0">Contact Settings</h5>
-                            <button type="button" class="btn btn-dark shadow-none btn-sm" data-bs-toggle="modal"
-                                data-bs-target="#contacts-s">
-                                <i class="bi bi-pencil-square"></i> Edit
-                            </button>
-                        </div>
-                        <div class="row">
-                            <!-- Contact setting column -->
-                            <div class="col-lg-6">
-                                <div class="mb-4">
-                                    <h6 class="card-subtitle mb-1 fw-bold">Address</h6>
-                                    <p class="card-text" id="address"></p>
-                                </div>
-                                <div class="mb-4">
-                                    <h6 class="card-subtitle mb-1 fw-bold">Google Map</h6>
-                                    <p class="card-text" id="gmap"></p>
-                                </div>
-                                <div class="mb-4">
-                                    <h6 class="card-subtitle mb-1 fw-bold">Phone Numbers</h6>
-                                    <p class="card-text mb-1">
-                                        <i class="bi bi-telephone-fill"></i>
-                                        <span id="pn1"></span>
-                                    </p>
-                                    <p class="card-text">
-                                        <i class="bi bi-telephone-fill"></i>
-                                        <span id="pn2"></span>
-                                    </p>
-                                </div>
-                                <div class="mb-4">
-                                    <h6 class="card-subtitle mb-1 fw-bold">E-mail</h6>
-                                    <p class="card-text" id="email"></p>
-                                </div>
-                            </div>
-                            <!-- Social Link column -->
-                            <div class="col-lg-6">
-                                <div class="mb-4">
-                                    <div class="mb-4">
-                                        <h6 class="card-subtitle mb-1 fw-bold">Social Links</h6>
-                                        <p class="card-text mb-1">
-                                            <i class="bi bi-facebook me-1"></i>
-                                            <span id="fb"></span>
-                                        </p>
-                                        <p class="card-text mb-1">
-                                            <i class="bi bi-instagram me-1"></i>
-                                            <span id="insta"></span>
-                                        </p>
-                                        <p class="card-text">
-                                            <i class="bi bi-twitter me-1"></i>
-                                            <span id="tw"></span>
-                                        </p>
-                                    </div>
-                                    <div class="mb-4">
-                                        <div class="mb-4">
-                                            <h6 class="card-subtitle mb-1 fw-bold">iFrame</h6>
-                                            <iframe id="iframe" class="border p-2 w-100" lodaing="lazy"></iframe>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-
-                    </div>
-                </div>
-                <!-- Contacts details Modal -->
-                <div class="modal fade" id="contacts-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1"
-                    aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-lg">
-                        <form id="contacts_s_form">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title">Contacts Settings</h5>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="container-fluid p-0">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <!-- Address -->
-                                                <div class="mb-3">
-                                                    <label class="form-label fw-bold">Address</label>
-                                                    <input type="text" name="address" id="address_input"
-                                                        class="form-control shadow-none" required>
-                                                </div>
-                                                <!-- Google map -->
-                                                <div class="mb-3">
-                                                    <label class="form-label fw-bold">Google Map link</label>
-                                                    <input type="text" name="gmap" id="gmap_input"
-                                                        class="form-control shadow-none" required>
-                                                </div>
-                                                <!-- Phone number -->
-                                                <div class="mb-3">
-                                                    <label class="form-label fw-bold">Phone Number (with country
-                                                        code)</label>
-                                                    <div class="input-group mb-3">
-                                                        <span class="input-group-text"><i
-                                                                class="bi bi-telephone-fill"></i></span>
-                                                        <input type="number" name="pn1" id="pn1_input"
-                                                            class="form-control shadow-none" required>
-                                                    </div>
-                                                    <div class="input-group mb-3">
-                                                        <span class="input-group-text"><i
-                                                                class="bi bi-telephone-fill"></i></span>
-                                                        <input type="number" name="pn2" id="pn2_input"
-                                                            class="form-control shadow-none">
-                                                    </div>
-                                                </div>
-                                                <!-- Email -->
-                                                <div class="mb-3">
-                                                    <label class="form-label fw-bold">Email</label>
-                                                    <input type="email" name="email" id="email_input"
-                                                        class="form-control shadow-none" required>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="mb-3">
-                                                    <label class="form-label fw-bold">Social Links</label>
-                                                    <div class="input-group mb-3">
-                                                        <span class="input-group-text"> <i class="bi bi-facebook">
-                                                            </i></span>
-                                                        <input type="text" name="fb" id="fb_input"
-                                                            class="form-control shadow-none" required>
-                                                    </div>
-                                                    <div class="input-group mb-3">
-                                                        <span class="input-group-text"><i class="bi bi-instagram">
-                                                            </i></span>
-                                                        <input type="text" name="insta" id="insta_input"
-                                                            class="form-control shadow-none" required>
-                                                    </div>
-                                                    <div class="input-group mb-3">
-                                                        <span class="input-group-text"><i class="bi bi-twitter">
-                                                            </i></span>
-                                                        <input type="text" name="tw" id="tw_input"
-                                                            class="form-control shadow-none">
-                                                    </div>
-
-                                                </div>
-                                                <!-- iFrame -->
-                                                <div class="mb-3">
-                                                    <label class="form-label fw-bold">iFrame Src</label>
-                                                    <input type="text" name="iframe" id="iframe_input"
-                                                        class="form-control shadow-none" required>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" onclick="contacts_input(contacts_data)"
-                                        class="btn text-secondary shadow-none" data-bs-dismiss="modal">CANCEL</button>
-                                    <button type="submit" class="btn custom-bg text-white shadow-none">SUBMIT</button>
-                                </div>
-                            </div>
-                        </form>
-
-                    </div>
-                </div>
-
-                <!-- Management Team Section -->
-                <div class="card border-0 shadow-sm mb-4">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center justify-content-between mb-3">
-                            <h5 class="card-title m-0">Management Team</h5>
-                            <button type="button" class="btn btn-dark shadow-none btn-sm" data-bs-toggle="modal"
-                                data-bs-target="#team-s">
+                                data-bs-target="#carousel-s">
                                 <i class="bi bi-plus-square"></i> Add
                             </button>
                         </div>
 
-                        <div class="row" id="team-data">
+                        <div class="row" id="carousel-data">
                         </div>
                     </div>
                 </div>
-                <!-- Management Team Modal -->
-                <div class="modal fade" id="team-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1"
+                <!-- CAROUSEL Modal -->
+                <div class="modal fade" id="carousel-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1"
                     aria-labelledby="staticBackdropLabel" aria-hidden="true">
                     <div class="modal-dialog">
                         <form id="team_s_form">
