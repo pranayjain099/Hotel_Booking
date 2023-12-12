40305f1b2a4862f7a0e79eae906cc5f931a1a430
pranayjain099 <pranayjain2001@gmail.com>
2023-12-13 02:36:18 +0530

    Admin, User_queries mark as read and delete query implemented

--- Admin/user_queries.php
+++ Admin/user_queries.php
@@ -2,7 +2,40 @@
 require('include/essentials.php');
 require('include/db_config.php');
 adminLogin();
-session_regenerate_id(true);
+
+// When click on mark as read
+if (isset($_GET['seen'])) {
+    $frm_data = filteration($_GET);
+
+    if ($frm_data['seen'] == 'all') {
+
+    } else {
+        $q = "UPDATE `user_queries` SET `seen`= ? WHERE `sr_no` = ?";
+        $values = [1, $frm_data['seen']];
+        if (update($q, $values, 'ii')) {
+            alert('success', 'Marked as read');
+        } else {
+            alert('error', 'Operation Failed');
+        }
+    }
+}
+
+// when click on del
+if (isset($_GET['del'])) {
+    $frm_data = filteration($_GET);
+
+    if ($frm_data['del'] == 'all') {
+
+    } else {
+        $q = "DELETE FROM `user_queries` WHERE `sr_no` = ?";
+        $values = [$frm_data['del']];
+        if (update($q, $values, 'i')) {
+            alert('success', 'Data Deleted!');
+        } else {
+            alert('error', 'Operation Failed');
+        }
+    }
+}
 ?>
 
 
@@ -30,6 +63,10 @@ session_regenerate_id(true);
 
                 <div class="card border-0 shadow-sm mb-4">
                     <div class="card-body">
+
+                        <div class="text-end mb-4">
+
+                        </div>
                         <div class="table-responsive-md" style="height: 450px; overflow-y: scroll;">
                             <table class="table table-hover border">
                                 <thead class="sticky-top">
@@ -38,7 +75,7 @@ session_regenerate_id(true);
                                         <th scope="col">Name</th>
                                         <th scope="col">Email</th>
                                         <th scope="col" width="20%">Subject</th>
-                                        <th scope="col" width="30%">Message</th>
+                                        <th scope="col" width="20%">Message</th>
                                         <th scope="col">Date</th>
                                         <th scope="col">Action</th>
                                     </tr>
@@ -51,6 +88,12 @@ session_regenerate_id(true);
                                     $i = 1;
 
                                     while ($row = mysqli_fetch_assoc($data)) {
+
+                                        $seen = '';
+                                        if ($row['seen'] != 1) {
+                                            $seen = "<a href='?seen=$row[sr_no]' class='btn btn-sm rounded-pill btn-primary'> Mark as read </a>";
+                                        }
+                                        $seen .= "<a href='?del=$row[sr_no]' class='btn btn-sm rounded-pill btn-danger mt-2'> Delete </a>";
                                         echo <<<query
                                             <tr>
                                                 <td>$i</td>
@@ -59,6 +102,7 @@ session_regenerate_id(true);
                                                 <td>$row[subject]</td>
                                                 <td>$row[message]</td>
                                                 <td>$row[date]</td>
+                                                <td>$seen</td>
                                             </tr>
                                             query;
                                         $i++;
