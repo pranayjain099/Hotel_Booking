e79962a86366a8737532ee8260b10ae4a48fd207
pranayjain099 <pranayjain2001@gmail.com>
2023-12-13 19:45:19 +0530

    Admin, Features & Facilities

--- Admin/features_facilities.php
+++ Admin/features_facilities.php
@@ -56,7 +56,7 @@ if (isset($_GET['del'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Queries</title>
+    <title>Features & Facilities</title>
     <?php
     require('include/links.php');
     ?>
@@ -70,18 +70,11 @@ if (isset($_GET['del'])) {
     <div class="container-fluid" id="main-content">
         <div class="row">
             <div class="col-lg-10 ms-auto p-4 pt-0 overflow-hidden">
-                <h3 class="mb-4">User Queries</h3>
+                <h3 class="mb-4">Features & Facilities</h3>
 
+                <!-- Table -->
                 <div class="card border-0 shadow-sm mb-4">
                     <div class="card-body">
-                        <!--Mark all read  -->
-                        <div class="text-end mb-4">
-                            <a href="?seen=all" class="btn btn-dark rounded-pill shadow-none btn-sm"><i
-                                    class="bi bi-check-all"></i> Mark all read</a>
-                            <!-- Delete all  -->
-                            <a href="?del=all" class="btn btn-danger rounded-pill shadow-none btn-sm"><i
-                                    class="bi bi-trash"></i> Delete all</a>
-                        </div>
                         <div class="table-responsive-md" style="height: 450px; overflow-y: scroll;">
                             <table class="table table-hover border">
                                 <thead class="sticky-top">
--- Admin/include/header.php
+++ Admin/include/header.php
@@ -21,6 +21,9 @@
                         <a class="nav-link text-white" href="dashboard.php">Dashboard</a>
                     </li>
                     <li class="nav-item">
+                        <a class="nav-link text-white" href="features_facilities.php">Features & Facilities</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link text-white" href="user_queries.php">User Queries</a>
                     </li>
                     <li class="nav-item">
