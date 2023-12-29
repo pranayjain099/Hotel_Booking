99631c23c5ec7fe98870e47b9c90c1d462d9b21b
pranayjain099 <pranayjain2001@gmail.com>
2023-12-29 22:46:20 +0530

    Two new tables created ,room_ Features and room_facilities

--- Admin/ajax/rooms.php
+++ Admin/ajax/rooms.php
@@ -11,8 +11,18 @@ if (isset($_POST['add_room'])) {
     $facilities = filteration(json_decode($_POST['facilities']));
 
     $frm_data = filteration($_POST);
+    $flag = 0;
 
     $q1 = "INSERT INTO `rooms`(`name`, `area`, `price`, `quantity`, `adult`, `children`, `description`) VALUES (?,?,?,?,?,?,?)";
+
+    $values = [$frm_data['name'], $frm_data['area'], $frm_data['price'], $frm_data['quanity'], $frm_data['adult'], $frm_data['children'], $frm_data['description']];
+
+    if (insert($q1, $values, 'siiiiis')) {
+        $flag = 1;
+    }
+
+    $room_id = mysqli_insert_id($con);
+    $q2 = "INSERT INTO `room_facilities`(`room_id`, `facilities_id`) VALUES (?,?)";
 
 
 }
